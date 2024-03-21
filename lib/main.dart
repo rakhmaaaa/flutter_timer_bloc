009b983abf830842e5f6cdfdcc08efaa972cdbb6
Rakhmadhani Nurul Aini <92674229+rakhmaaaa@users.noreply.github.com>
2024-03-21 21:59:49 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_timer_bloc/app.dart';
+
+void main() => runApp(const App());
---
+++ lib/timer/view/timer_page.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_timer_bloc/ticker.dart';
+import 'package:flutter_timer_bloc/timer/timer.dart';
+
+class TimerPage extends StatelessWidget {
+  const TimerPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Timer Page'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Background(),
+          const SizedBox(height: 20),
+          const Actions(),
+        ],
+      ),
+    );
+  }
+}
+
+class Actions extends StatelessWidget {
+  const Actions({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<TimerBloc, TimerState>(
+      buildWhen: (prev, state) => prev.runtimeType != state.runtimeType,
+      builder: (context, state) {
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ...switch (state) {
+              TimerInitial() => [
+                  FloatingActionButton(
+                    child: const Icon(Icons.play_arrow),
+                    onPressed: () => context
+                        .read<TimerBloc>()
+                        .add(TimerStarted(duration: state.duration)),
+                  ),
+                ],
+              TimerRunInProgress() => [
+                  FloatingActionButton(
+                    child: const Icon(Icons.pause),
+                    onPressed: () =>
+                        context.read<TimerBloc>().add(const TimerPaused()),
+                  ),
+                  FloatingActionButton(
+                    child: const Icon(Icons.replay),
+                    onPressed: () =>
+                        context.read<TimerBloc>().add(const TimerReset()),
+                  ),
+                ],
+              TimerRunPause() => [
+                  FloatingActionButton(
+                    child: const Icon(Icons.play_arrow),
+                    onPressed: () =>
+                        context.read<TimerBloc>().add(const TimerResumed()),
+                  ),
+                  FloatingActionButton(
+                    child: const Icon(Icons.replay),
+                    onPressed: () =>
+                        context.read<TimerBloc>().add(const TimerReset()),
+                  ),
+                ],
+              TimerRunComplete() => [
+                  FloatingActionButton(
+                    child: const Icon(Icons.replay),
+                    onPressed: () =>
+                        context.read<TimerBloc>().add(const TimerReset()),
+                  ),
+                ]
+            }
+          ],
+        );
+      },
+    );
+  }
+}
+
+class Background extends StatelessWidget {
+  const Background({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Colors.blue.shade50,
+            Colors.blue.shade500,
+          ],
+        ),
+      ),
+    );
+  }
+}
